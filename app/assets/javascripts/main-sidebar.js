9b353f84ac66489bfe5a0e1c06b369181e0c59c2
Тимурлан Рахимбердиев <rahimberdiev.t@fcb.local>
2016-07-22 12:41:24 +0600

    update main sidebar

--- app/assets/javascripts/main-sidebar.js
+++ app/assets/javascripts/main-sidebar.js
@@ -1,17 +1,32 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require bootstrap-sprockets
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+$(document).ready(function () {
+  var trigger = $('.menu-toggle'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+    $('.overlay,.sidebar-brand').click(function () {
+      $("#wrapper").removeClass("toggled");
+      overlay.hide();
+        trigger.removeClass('toggled');
+        trigger.addClass('is-closed');
+        isClosed = false;
+    });
+
+    function hamburger_cross() {
+        $("#wrapper").toggleClass("toggled");
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('toggled');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('toggled');
+        isClosed = true;
+      }
+  }  
+});
