5ddb18bcd07e872786d88831fc81de40578b8b77
LAGRANJ <rahimberdiev@gmail.com>
2016-08-06 15:11:00 +0600

    add uuid

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel_#{:uuid}"
+    stream_from "room_channel_#{uuid}"
   end
 
   def unsubscribed
@@ -9,6 +9,6 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-  	ActionCable.server.broadcast "room_channel_#{:uuid}", message: data['message']
+  	ActionCable.server.broadcast "room_channel_#{uuid}", message: data['message']
   end
 end
