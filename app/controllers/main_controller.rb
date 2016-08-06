fab995621b49df0b49f61bbd4783aaa91a859be3
LAGRANJ <rahimberdiev@gmail.com>
2016-08-06 13:02:30 +0600

    change redis password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,7 +5,7 @@ class MainController < ApplicationController
     @newsitems = Newsitem.where(priority:1)
     @records = ActiveRecord::Base.connection.execute("select * from get_currency_rates(840)")
     redis = Redis.new
-    redis = Redis.new(:host => "192.168.1.241", :port => 6379, :password => '12345')
+    redis = Redis.new(:host => "192.168.1.241", :port => 6379)
     @redis_message = redis.get("test")
   end
 end
