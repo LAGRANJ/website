b92c179e4a811fa6729738a5098bd01696447326
LAGRANJ <rahimberdiev@gmail.com>
2016-08-06 11:24:30 +0600

    add actioncable

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,11 @@
+require 'redis'
 class MainController < ApplicationController
   def index
   	@mainnewsitem = Newsitem.where(priority:0).first
     @newsitems = Newsitem.where(priority:1)
     @records = ActiveRecord::Base.connection.execute("select * from get_currency_rates(840)")
-
+    redis = Redis.new
+    redis = Redis.new(:host => "192.168.1.241", :port => 6379, :password => '12345')
+    @redis_message = redis.get("test")
   end
 end
